516450ee341c7b1ec07f55ec7e17d7ceafbc1fd7
RidaRidss <ridarocks12@yahoo.com>
2015-08-25 17:19:50 +0500

    commit

--- task 1/app.js
+++ task 1/app.js
@@ -1,30 +1,27 @@
 /**
  * Created by Rida on 8/24/2015.
  */
-var num1;
-var num2;
-var result;
 function submit_Operation_plus() {
-    num1 = document.getElementById('number1')['value'];
-    num2 = document.getElementById('number2')['value'];
-    result = num1 + num2;
-    alert('your ist entered digit : ' + num1 + '\n \n your 2nd entered digit :' + num2 + " \n \n you have pressed button for operation : plus");
+    var num1 = Number(document.getElementById('number1')['value']);
+    var num2 = Number(document.getElementById('number2')['value']);
+    var result = num1 + num2;
+    alert('your ist entered digit : ' + num1 + '\n \n your 2nd entered digit :' + num2 + " \n \n your result : " + result);
 }
 function submit_Operation_minus() {
-    num1 = document.getElementById('number1')['value'];
-    num2 = document.getElementById('number2')['value'];
-    result = num1 - num2;
-    alert('your ist entered digit : ' + num1 + '\n \n your 2nd entered digit :' + num2 + "\n \n you have pressed button for operation : minus");
+    var num1 = Number(document.getElementById('number1')['value']);
+    var num2 = Number(document.getElementById('number2')['value']);
+    var result = num1 - num2;
+    alert('your ist entered digit : ' + num1 + '\n \n your 2nd entered digit :' + num2 + "\n \n your result : " + result);
 }
 function submit_Operation_multiply() {
-    num1 = document.getElementById('number1')['value'];
-    num2 = document.getElementById('number2')['value'];
-    result = num1 * num2;
-    alert('your ist entered digit : ' + num1 + '\n \n your 2nd entered digit :' + num2 + "\n \n you have pressed button for operation : multiply");
+    var num1 = Number(document.getElementById('number1')['value']);
+    var num2 = Number(document.getElementById('number2')['value']);
+    var result = num1 * num2;
+    alert('your ist entered digit : ' + num1 + '\n \n your 2nd entered digit :' + num2 + "\n \n your result : " + result);
 }
 function submit_Operation_divide() {
-    num1 = document.getElementById('number1')['value'];
-    num2 = document.getElementById('number2')['value'];
-    result = num1 / num2;
-    alert('your ist entered digit : ' + num1 + '\n \n your 2nd entered digit :' + num2 + "\n \n you have pressed button for operation : divide");
+    var num1 = Number(document.getElementById('number1')['value']);
+    var num2 = Number(document.getElementById('number2')['value']);
+    var result = num1 / num2;
+    alert('your ist entered digit : ' + num1 + '\n \n your 2nd entered digit :' + num2 + "\n \n your result : " + result);
 }
