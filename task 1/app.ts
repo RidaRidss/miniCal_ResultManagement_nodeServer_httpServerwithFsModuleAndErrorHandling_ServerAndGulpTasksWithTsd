2c342718c4ef328d46a540765cdb30fc0e4055b1
RidaRidss <ridarocks12@yahoo.com>
2015-08-25 17:22:10 +0500

    commit

--- task 1/app.ts
+++ task 1/app.ts
@@ -3,26 +3,26 @@
  */
 
 function submit_Operation_plus(){
-   let num1 =  Number(document.getElementById('number1')['value']);
-  let  num2 = Number(document.getElementById('number2')['value']);
- let result =  num1 + num2;
+   let num1 : number =  Number(document.getElementById('number1')['value']);
+  let  num2 : number  = Number(document.getElementById('number2')['value']);
+ let result : number  =  num1 + num2;
     alert('your ist entered digit : '+ num1 + '\n \n your 2nd entered digit :' + num2 + " \n \n your result : " + result);
 }
 function submit_Operation_minus(){
- let   num1 =  Number(document.getElementById('number1')['value']);
-   let num2 = Number(document.getElementById('number2')['value']);
-   let result =  num1 - num2;
+ let   num1 : number  =  Number(document.getElementById('number1')['value']);
+   let num2 : number  = Number(document.getElementById('number2')['value']);
+   let result : number  =  num1 - num2;
     alert('your ist entered digit : '+ num1 + '\n \n your 2nd entered digit :' + num2 + "\n \n your result : " + result);
 }
 function submit_Operation_multiply(){
- let   num1 =  Number(document.getElementById('number1')['value']);
- let   num2 = Number(document.getElementById('number2')['value']);
- let   result =  num1 * num2;
+ let   num1  : number =  Number(document.getElementById('number1')['value']);
+ let   num2 : number  = Number(document.getElementById('number2')['value']);
+ let   result : number  =  num1 * num2;
     alert('your ist entered digit : '+ num1 + '\n \n your 2nd entered digit :' + num2  + "\n \n your result : " + result);
 }
 function submit_Operation_divide(){
- let   num1 =  Number(document.getElementById('number1')['value']);
- let   num2 = Number(document.getElementById('number2')['value']);
- let   result =  num1 / num2;
+ let   num1 : number  =  Number(document.getElementById('number1')['value']);
+ let   num2 : number  = Number(document.getElementById('number2')['value']);
+ let   result : number  =  num1 / num2;
     alert('your ist entered digit : '+ num1 + '\n \n your 2nd entered digit :' + num2  + "\n \n your result : " + result);
 }
