7ee2d4cccd4cc276d59ae02453418c6841ab659a
RidaRidss <ridarocks12@yahoo.com>
2015-08-25 20:41:34 +0500

    commit

---
+++ task 2/app.ts
@@ -0,0 +1,43 @@
+function Save(){
+    let sno :number = 0;
+    let a;
+    let tableData = [];
+    let table;
+    sno +=1;
+    a=document.getElementById('resultDiv');
+    a.style.display="Block";
+    tableData=[document.createElement("td"),document.createElement("td"),document.createElement("td")];
+    let tableRow = document.createElement('tr');
+    table=document.getElementById("student");
+    table.appendChild(tableRow);
+    tableRow.appendChild(tableData[0]);
+    tableRow.appendChild(tableData[1]);
+    tableRow.appendChild(tableData[2]);
+    tableData[0].innerHTML=sno;
+    if (document.getElementById('n')['value'] == '' || document.getElementById('n')['value'] == ' ') {
+        var nameValue = "Null value";
+        alert("error : Please Check Your Ist Input Value \n  Check Error In Below Table");
+        tableData[1].innerHTML = nameValue;
+    }
+    else if (document.getElementById('n')['value'] == Number(document.getElementById('n')['value'])) {
+        var nameValue = "Invalid Name";
+        alert("error : Please Check Your Ist Value \n Check Error In Below Table");
+        tableData[1].innerHTML = nameValue;
+    }
+    else {
+        tableData[1].innerHTML = document.getElementById('n')['value'];
+    }
+    if (document.getElementById('a')['value'] == '' || document.getElementById('a')['value'] == ' ') {
+        var idValue = "Null value";
+        alert("error : Please Check Your 2nd Value \n Check Error In Below Table");
+        tableData[2].innerHTML = idValue;
+    }
+    else if (document.getElementById('a')['value'] != Number(document.getElementById('a')['value'])) {
+        var idValue = "Invalid Id";
+        alert("error : Please Check Your 2nd Value \n  Check Error In Below Table");
+        tableData[2].innerHTML = idValue;
+    }
+    else {
+        tableData[2].innerHTML = document.getElementById('a')['value'];
+    }
+}
